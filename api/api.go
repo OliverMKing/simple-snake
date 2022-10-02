65235e3adf20f62322e8ca6f3eb1a382b5df3440
Oliver King <olivermerkleyking@gmail.com>
2022-10-02 17:28:31 -0400

    refactor server

--- api/api.go
+++ api/api.go
@@ -7,34 +7,18 @@ import (
 	"simple-snake/snake"
 )
 
-type Api interface {
-	Run()
+func New(s snake.Snake) http.Handler {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", infoHandler(s))
+	return mux
 }
 
-type api struct {
-	snake snake.Snake
-	port  string
-}
-
-var _ Api = &api{}
-
-func New(s snake.Snake, port string) Api {
-	return &api{
-		snake: s,
-		port:  port,
-	}
-}
-
-func (a *api) Run() {
-	http.HandleFunc("/", a.infoHandler)
-	log.Fatal(http.ListenAndServe(":"+a.port, nil))
-}
-
-func (a *api) infoHandler(w http.ResponseWriter, req *http.Request) {
-	resp := a.snake.Info()
-
-	w.Header().Set("Content-Type", "application/json")
-	if err := json.NewEncoder(w).Encode((resp)); err != nil {
-		log.Printf("failed to encode: %s", err)
+func infoHandler(s snake.Snake) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		resp := s.Info()
+		w.Header().Set("Content-Type", "application/json")
+		if err := json.NewEncoder(w).Encode((resp)); err != nil {
+			log.Printf("failed to encode: %s", err)
+		}
 	}
 }
--- main.go
+++ main.go
@@ -1,12 +1,45 @@
 package main
 
 import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
 	"simple-snake/api"
 	"simple-snake/snake"
+	"syscall"
+	"time"
 )
 
 func main() {
 	s := snake.New()
-	server := api.New(s, "8080")
-	server.Run()
+	h := api.New(s)
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: h,
+	}
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("server failed: %s", err.Error())
+		}
+	}()
+	log.Printf("server started on: %s", srv.Addr)
+
+	<-done
+	log.Print("server stopping")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer func() {
+		cancel()
+	}()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("server shutdown failed: %s", err.Error())
+	}
+	log.Print("server stopped")
 }
